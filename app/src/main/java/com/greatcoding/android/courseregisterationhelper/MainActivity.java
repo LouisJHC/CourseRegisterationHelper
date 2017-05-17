368f72770a2de30f39ac7bd421539b3659e63642
Louis Cha <lcha@sfu.ca>
2017-05-16 18:49:52 -0700

    Designed main page and added fragments.

--- app/src/main/java/com/greatcoding/android/courseregisterationhelper/MainActivity.java
+++ app/src/main/java/com/greatcoding/android/courseregisterationhelper/MainActivity.java
@@ -1,13 +1,70 @@
 package com.greatcoding.android.courseregisterationhelper;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    LinearLayout linear;
+    Button coursesButton, timeButton, statButton;
+    CoursesFragment fragment1;
+    TimeFragment fragment2;
+    StatFragment fragment3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        coursesButton = (Button) findViewById(R.id.coursesButton);
+        timeButton = (Button) findViewById(R.id.timeButton);
+        statButton = (Button) findViewById(R.id.statButton);
+        linear = (LinearLayout) findViewById(R.id.linear);
+        fragment1 = new CoursesFragment();
+        fragment2 = new TimeFragment();
+        fragment3 = new StatFragment();
+
+        coursesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linear.setVisibility(View.GONE);
+                coursesButton.setBackgroundColor(getResources().getColor(R.color.color8));
+                timeButton.setBackgroundColor(getResources().getColor(R.color.color9));
+                statButton.setBackgroundColor(getResources().getColor(R.color.color9));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, fragment1).commit();
+            }
+        });
+
+        timeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linear.setVisibility(View.GONE);
+                coursesButton.setBackgroundColor(getResources().getColor(R.color.color9));
+                timeButton.setBackgroundColor(getResources().getColor(R.color.color8));
+                statButton.setBackgroundColor(getResources().getColor(R.color.color9));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, fragment2).commit();
+            }
+        });
+
+        statButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linear.setVisibility(View.GONE);
+                coursesButton.setBackgroundColor(getResources().getColor(R.color.color9));
+                timeButton.setBackgroundColor(getResources().getColor(R.color.color9));
+                statButton.setBackgroundColor(getResources().getColor(R.color.color8));
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragment, fragment3).commit();
+            }
+        });
     }
 }
